ff1cd63a5b6c86f49d99fb5051b12bfa01d394bb
tommaso_tombacco <tommaso.tombacco@studenti.unipd.it>
2025-11-05 17:27:45 +0100

    Fix script menù mobile

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu');
-    const menuList = menuToggle.querySelector('ul');
-    menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('open');
-        if(menuToggle.classList.contains('open')){
-            menuList.classList.add('show');
-        }
-    });
+  const menuToggle = document.getElementById('menu');
+  const menuList = menuToggle.querySelector('ul');
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('open');
+    if (menuToggle.classList.contains('open')) {
+      menuList.classList.add('show');
+    } else {
+      menuList.classList.remove('show');
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 980) {
+      menuToggle.classList.remove('open');
+      menuList.classList.remove('show');
+    }
+  });
 });
